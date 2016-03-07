1140c418b74fce33dc4502f521ccc6f8687fecdb
lpdemilis <lpdemilis@gmail.com>
2016-03-07 18:45:07 -0300

    Commit

---
+++ Reservas/grails-app/taglib/br/com/reservas/LoginTagLib.groovy
@@ -0,0 +1,16 @@
+package br.com.reservas
+
+
+class LoginTagLib {
+	def springSecurityService
+	
+	def loginControl = {
+		if(springSecurityService.isLoggedIn()){
+			out << "${springSecurityService.currentUser} "
+			out << """[${link(action:"index", controller:"logout"){"Logout"}}]"""
+		} else {
+		 	//out << """[${link(action:"index", controller:"usuario"){"Login"}}]"""
+			out << """[${link(action:"auth", controller:"login"){"Login"}}]"""
+		}
+	}
+}
