f4c6cf846da9fcc2bd344e0d6175ebe4416fffaa
lpdemilis <lpdemilis@gmail.com>
2016-03-11 19:02:52 -0300

    Commit

--- Reservas/grails-app/controllers/br/com/reservas/MensalidadeController.groovy
+++ Reservas/grails-app/controllers/br/com/reservas/MensalidadeController.groovy
@@ -1,5 +1,107 @@
 package br.com.reservas
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class MensalidadeController {
-    static scaffold = true
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [mensalidadeInstanceList: Mensalidade.list(params), mensalidadeInstanceTotal: Mensalidade.count()]
+    }
+
+    def create() {
+        [mensalidadeInstance: new Mensalidade(params)]
+    }
+
+    def save() {
+        def mensalidadeInstance = new Mensalidade(params)
+		
+		def boletoInstance = new Boleto(params)
+		boletoInstance.save()
+		mensalidadeInstance.boleto = boletoInstance
+		
+        if (!mensalidadeInstance.save(flush: true)) {
+            render(view: "create", model: [mensalidadeInstance: mensalidadeInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.created.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), mensalidadeInstance.id])
+        redirect(action: "show", id: mensalidadeInstance.id)
+    }
+
+    def show(Long id) {
+        def mensalidadeInstance = Mensalidade.get(id)
+        if (!mensalidadeInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [mensalidadeInstance: mensalidadeInstance]
+    }
+
+    def edit(Long id) {
+        def mensalidadeInstance = Mensalidade.get(id)
+        if (!mensalidadeInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [mensalidadeInstance: mensalidadeInstance]
+    }
+
+    def update(Long id, Long version) {
+        def mensalidadeInstance = Mensalidade.get(id)
+        if (!mensalidadeInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (mensalidadeInstance.version > version) {
+                mensalidadeInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'mensalidade.label', default: 'Mensalidade')] as Object[],
+                          "Another user has updated this Mensalidade while you were editing")
+                render(view: "edit", model: [mensalidadeInstance: mensalidadeInstance])
+                return
+            }
+        }
+
+        mensalidadeInstance.properties = params
+
+        if (!mensalidadeInstance.save(flush: true)) {
+            render(view: "edit", model: [mensalidadeInstance: mensalidadeInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), mensalidadeInstance.id])
+        redirect(action: "show", id: mensalidadeInstance.id)
+    }
+
+    def delete(Long id) {
+        def mensalidadeInstance = Mensalidade.get(id)
+        if (!mensalidadeInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            mensalidadeInstance.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'mensalidade.label', default: 'Mensalidade'), id])
+            redirect(action: "show", id: id)
+        }
+    }
 }
--- Reservas/test/unit/br/com/reservas/MensalidadeControllerTests.groovy
+++ Reservas/test/unit/br/com/reservas/MensalidadeControllerTests.groovy
@@ -2,16 +2,154 @@ package br.com.reservas
 
 
 
-import grails.test.mixin.*
 import org.junit.*
+import grails.test.mixin.*
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(MensalidadeController)
+@Mock(Mensalidade)
 class MensalidadeControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    def populateValidParams(params) {
+        assert params != null
+        // TODO: Populate valid properties like...
+        //params["name"] = 'someValidName'
+    }
+
+    void testIndex() {
+        controller.index()
+        assert "/mensalidade/list" == response.redirectedUrl
+    }
+
+    void testList() {
+
+        def model = controller.list()
+
+        assert model.mensalidadeInstanceList.size() == 0
+        assert model.mensalidadeInstanceTotal == 0
+    }
+
+    void testCreate() {
+        def model = controller.create()
+
+        assert model.mensalidadeInstance != null
+    }
+
+    void testSave() {
+        controller.save()
+
+        assert model.mensalidadeInstance != null
+        assert view == '/mensalidade/create'
+
+        response.reset()
+
+        populateValidParams(params)
+        controller.save()
+
+        assert response.redirectedUrl == '/mensalidade/show/1'
+        assert controller.flash.message != null
+        assert Mensalidade.count() == 1
+    }
+
+    void testShow() {
+        controller.show()
+
+        assert flash.message != null
+        assert response.redirectedUrl == '/mensalidade/list'
+
+        populateValidParams(params)
+        def mensalidade = new Mensalidade(params)
+
+        assert mensalidade.save() != null
+
+        params.id = mensalidade.id
+
+        def model = controller.show()
+
+        assert model.mensalidadeInstance == mensalidade
+    }
+
+    void testEdit() {
+        controller.edit()
+
+        assert flash.message != null
+        assert response.redirectedUrl == '/mensalidade/list'
+
+        populateValidParams(params)
+        def mensalidade = new Mensalidade(params)
+
+        assert mensalidade.save() != null
+
+        params.id = mensalidade.id
+
+        def model = controller.edit()
+
+        assert model.mensalidadeInstance == mensalidade
+    }
+
+    void testUpdate() {
+        controller.update()
+
+        assert flash.message != null
+        assert response.redirectedUrl == '/mensalidade/list'
+
+        response.reset()
+
+        populateValidParams(params)
+        def mensalidade = new Mensalidade(params)
+
+        assert mensalidade.save() != null
+
+        // test invalid parameters in update
+        params.id = mensalidade.id
+        //TODO: add invalid values to params object
+
+        controller.update()
+
+        assert view == "/mensalidade/edit"
+        assert model.mensalidadeInstance != null
+
+        mensalidade.clearErrors()
+
+        populateValidParams(params)
+        controller.update()
+
+        assert response.redirectedUrl == "/mensalidade/show/$mensalidade.id"
+        assert flash.message != null
+
+        //test outdated version number
+        response.reset()
+        mensalidade.clearErrors()
+
+        populateValidParams(params)
+        params.id = mensalidade.id
+        params.version = -1
+        controller.update()
+
+        assert view == "/mensalidade/edit"
+        assert model.mensalidadeInstance != null
+        assert model.mensalidadeInstance.errors.getFieldError('version')
+        assert flash.message != null
+    }
+
+    void testDelete() {
+        controller.delete()
+        assert flash.message != null
+        assert response.redirectedUrl == '/mensalidade/list'
+
+        response.reset()
+
+        populateValidParams(params)
+        def mensalidade = new Mensalidade(params)
+
+        assert mensalidade.save() != null
+        assert Mensalidade.count() == 1
+
+        params.id = mensalidade.id
+
+        controller.delete()
+
+        assert Mensalidade.count() == 0
+        assert Mensalidade.get(mensalidade.id) == null
+        assert response.redirectedUrl == '/mensalidade/list'
     }
 }
