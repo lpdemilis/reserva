c8eb981a3f48be1adcbbf5a69b2b41f48a916976
lpdemilis <lpdemilis@gmail.com>
2016-03-21 23:25:49 -0300

    Update BuildConfig.groovy

--- Reservas/grails-app/conf/BuildConfig.groovy
+++ Reservas/grails-app/conf/BuildConfig.groovy
@@ -36,6 +36,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "https://repo.grails.org/grails/plugins"
     }
 
     dependencies {
