1a55e5991f0ac7099613d3af81225b1535ba8b3d
lpdemilis <lpdemilis@gmail.com>
2016-03-29 23:41:00 -0300

    Commit

--- Reservas/grails-app/conf/BootStrap.groovy
+++ Reservas/grails-app/conf/BootStrap.groovy
@@ -1,12 +1,90 @@
+import grails.plugins.springsecurity.SpringSecurityService
 import br.com.reservas.Cidade
 import br.com.reservas.Condominio
 import br.com.reservas.Endereco
+import br.com.reservas.Papel
 import br.com.reservas.Recurso
 import br.com.reservas.TipoReserva
+import br.com.reservas.Usuario
+import br.com.reservas.UsuarioPapel
 
 class BootStrap {
+	
+	SpringSecurityService springSecurityService = new SpringSecurityService();
 
     def init = { servletContext ->
+		/** Papel **/
+		Papel papelInstanceUser = Papel.findByAuthority("ROLE_USER");
+		if (papelInstanceUser == null) {
+			papelInstanceUser = new Papel();
+			papelInstanceUser.authority = "ROLE_USER";
+			
+			papelInstanceUser.save(flush: true);
+		}
+		
+		Papel papelInstanceAdmin = Papel.findByAuthority("ROLE_ADMIN");
+		if (papelInstanceAdmin == null) {
+			papelInstanceAdmin = new Papel();
+			papelInstanceAdmin.authority = "ROLE_ADMIN";
+			
+			papelInstanceAdmin.save(flush: true);
+		}
+		/** Papel **/
+		
+		/** Ususário **/
+		Usuario usuarioInstanceAdmin = Usuario.get(1);
+		if (usuarioInstanceAdmin == null) {
+			usuarioInstanceAdmin = new Usuario();
+			usuarioInstanceAdmin.nome = 'Administrador';
+			usuarioInstanceAdmin.email = 'administrador@email.com.br';
+			usuarioInstanceAdmin.username = 'administrador@email.com.br';
+			usuarioInstanceAdmin.password = springSecurityService.encodePassword("123456");
+			usuarioInstanceAdmin.enabled = true;
+			usuarioInstanceAdmin.accountExpired = false;
+			usuarioInstanceAdmin.accountLocked = false;
+			usuarioInstanceAdmin.passwordExpired = false;
+			
+			usuarioInstanceAdmin.save(flush: true);			
+		}
+		
+		/** Papel Usuário **/
+		UsuarioPapel usuarioPapelInstanceAdmin = UsuarioPapel.findByPapelAndUsuario(papelInstanceAdmin, usuarioInstanceAdmin);
+		if (usuarioPapelInstanceAdmin == null) {
+			usuarioPapelInstanceAdmin = new UsuarioPapel();
+			usuarioPapelInstanceAdmin.usuario = usuarioInstanceAdmin;
+			usuarioPapelInstanceAdmin.papel = papelInstanceAdmin;
+			
+			usuarioPapelInstanceAdmin.save(flush: true);
+		}
+		/** Papel Usuário **/
+		
+		Usuario usuarioInstanceUser = Usuario.get(2);
+		if (usuarioInstanceUser == null) {
+			usuarioInstanceUser = new Usuario();
+			usuarioInstanceUser.nome = 'Usu\u00E1rio';
+			usuarioInstanceUser.email = 'usuario@email.com.br';
+			usuarioInstanceUser.username = 'usuario@email.com.br';
+			usuarioInstanceUser.password = springSecurityService.encodePassword("123456");
+			usuarioInstanceUser.enabled = true;
+			usuarioInstanceUser.accountExpired = false;
+			usuarioInstanceUser.accountLocked = false;
+			usuarioInstanceUser.passwordExpired = false;
+			
+			usuarioInstanceUser.save(flush: true);
+		}		
+		
+		/** Papel Usuário **/
+		UsuarioPapel usuarioPapelInstanceUser = UsuarioPapel.findByPapelAndUsuario(papelInstanceUser, usuarioInstanceUser);
+		if (usuarioPapelInstanceUser == null) {
+			usuarioPapelInstanceUser = new UsuarioPapel();
+			usuarioPapelInstanceUser.usuario = usuarioInstanceUser;
+			usuarioPapelInstanceUser.papel = papelInstanceUser;
+		
+			usuarioPapelInstanceUser.save(flush: true);
+		}
+		/** Papel Usuário **/
+		/** Usuário **/
+				
 		/** Condominios **/
 		Condominio condominioInstance = Condominio.findByNome("Achilles Ceccato");
 		if (condominioInstance == null) {
@@ -20,10 +98,10 @@ class BootStrap {
 			enderecoInstance.bairro = "Itacorubi";
 			enderecoInstance.cep = "88034-600";
 			enderecoInstance.cidade = Cidade.findByNome("Florian\u00F3polis");
-			enderecoInstance.save(flush: true)
+			enderecoInstance.save(flush: true);
 			
 			condominioInstance.endereco = enderecoInstance;
-			condominioInstance.save(flush: true) 			
+			condominioInstance.save(flush: true); 			
 		}
 		
 		/** Recursos **/
@@ -34,7 +112,7 @@ class BootStrap {
 			recursoInstance.descricao = "";			
 			recursoInstance.condominio = condominioInstance;
 			
-			recursoInstance.save(flush: true)
+			recursoInstance.save(flush: true);
 		}
 		/** Recursos **/
 		
@@ -50,10 +128,10 @@ class BootStrap {
 			enderecoInstance.bairro = "Centro";
 			enderecoInstance.cep = "88034-000";
 			enderecoInstance.cidade = Cidade.findByNome("Florian\u00F3polis");
-			enderecoInstance.save(flush: true)
+			enderecoInstance.save(flush: true);
 			
 			condominioInstance.endereco = enderecoInstance;
-			condominioInstance.save(flush: true)			
+			condominioInstance.save(flush: true);			
 		}
 		
 		/** Recursos **/
@@ -64,7 +142,7 @@ class BootStrap {
 			recursoInstance.descricao = "";
 			recursoInstance.condominio = condominioInstance;
 			
-			recursoInstance.save(flush: true)
+			recursoInstance.save(flush: true);
 		}
 		/** Recursos **/		
 		/** Condominios **/
