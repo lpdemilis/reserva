befc0db85a268e9d756c27a74ef9b3a3705e9ced
lpdemilis <lpdemilis@gmail.com>
2016-05-09 14:56:08 -0300

    Commit

---
+++ Reservas/grails-app/domain/br/com/reservas/UnidadeTempoReserva.groovy
@@ -0,0 +1,15 @@
+package br.com.reservas
+
+class UnidadeTempoReserva {
+	
+	String nome
+	String descricao
+
+    static constraints = {
+		nome nullable:false, blank: false 
+    }
+	
+	String toString(){
+		nome
+	}
+}
